e8a6dbac46124480aa17743ba13f48fceaefb4ac
Kevin Mees <kev.mees@gmail.com>
2022-12-02 15:48:51 +0100

    chore: update prettier

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -2,23 +2,20 @@ import run from "aocrunner"
 import { map, pipe, sum } from "ramda"
 import { splitLines, splitPair, toCharCode } from "../utils/index.js"
 
-const normalize = (char: string) =>
-  toCharCode(char) - toCharCode(char >= "X" ? "X" : "A")
+const normalize = (char: string) => toCharCode(char) - toCharCode(char >= "X" ? "X" : "A")
 const prev = (n: number) => (n + 2) % 3
 const next = (n: number) => (n + 1) % 3
 
 const parseInput = (rawInput: string) =>
   pipe(splitLines, map(pipe(splitPair, map(normalize))))(rawInput)
 
-const score = ([op, me]: number[]) =>
-  me + 1 + 3 * (me === next(op) ? 2 : +(me === op))
+const score = ([op, me]: number[]) => me + 1 + 3 * (me === next(op) ? 2 : +(me === op))
 
 const part1 = (rawInput: string) => pipe(map(score), sum)(parseInput(rawInput))
 
 const choosePlay = ([op, me]: number[]) => [op, [prev(op), op, next(op)][me]]
 
-const part2 = (rawInput: string) =>
-  pipe(map(choosePlay), map(score), sum)(parseInput(rawInput))
+const part2 = (rawInput: string) => pipe(map(choosePlay), map(score), sum)(parseInput(rawInput))
 
 run({
   part1: {
