2aab918cf2c0d545d39f27ea780b51ee19f830a1
Kevin Mees <kev.mees@gmail.com>
2022-12-03 14:10:57 +0100

    day 3: done

---
+++ src/day03/index.ts
@@ -0,0 +1,81 @@
+import run from "aocrunner"
+import {
+  apply,
+  chain,
+  compose,
+  converge,
+  divide,
+  head,
+  identity,
+  intersection,
+  length,
+  map,
+  pipe,
+  reduce,
+  splitAt,
+  splitEvery,
+  sum,
+  __,
+} from "ramda"
+import { splitLines, toCharCode } from "../utils/index.js"
+
+const parseInput = (rawInput: string) => splitLines(rawInput)
+
+const toPriority = (charCode: number) => charCode - (charCode >= toCharCode("a") ? 96 : 38)
+
+const part1 = (rawInput: string) => {
+  return pipe(
+    map<string, [string, string]>(converge(splitAt, [compose(divide(__, 2), length), identity])),
+    chain(apply(intersection)),
+    map(toCharCode),
+    map(toPriority),
+    sum,
+  )(parseInput(rawInput))
+}
+
+const part2 = (rawInput: string) => {
+  return pipe(
+    splitEvery(3),
+    chain((chunk) => reduce(intersection, head(chunk), chunk)),
+    map(toCharCode),
+    map(toPriority),
+    sum,
+  )(parseInput(rawInput))
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          vJrwpWtwJgWrhcsFMMfFFhFp
+          jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+          PmmdzqPrVvPwwTWBwg
+          wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+          ttgJtRGJQctTZtZT
+          CrZsJsPPZsGzwwsLwLmpwMDw
+        `,
+        expected: 157,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+      `,
+        expected: 70,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
